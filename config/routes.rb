c035a0a8f181c629625bb9faa79e2862fb44549e
Juha-Pekka Moilanen <juha-pekka.moilanen@helsinki.fi>
2017-01-25 17:31:45 +0200

    Week 2, exercise 1

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   resources :beers
   resources :breweries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'breweries#index' # = get '/', to: 'breweries#index'
+
+  get 'kaikki_bisset', to: 'beers#index' # = get( 'kaikki_bisset', { :to => 'beers#index' } )
+
+  get 'ratings', to: 'ratings#index'
+
 end
