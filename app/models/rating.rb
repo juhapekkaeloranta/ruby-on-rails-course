8bdefa89cfb30c4f4159db77a0c2f0547f4f9e94
Juha-Pekka Moilanen <juha-pekka.moilanen@helsinki.fi>
2017-01-28 13:29:06 +0200

    Add ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,7 @@
 class Rating < ApplicationRecord
+  belongs_to :beer
+
+  def to_s
+    Beer.find(self.beer_id.to_s).name + " " + self.score.to_s
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-b1 = Brewery.create name:"Koff", year:1897
-b2 = Brewery.create name:"Malmgard", year:2001
-b3 = Brewery.create name:"Weihenstephaner", year:1042
+brew1 = Brewery.create name:"Koff", year:1897
+brew2 = Brewery.create name:"Malmgard", year:2001
+brew3 = Brewery.create name:"Weihenstephaner", year:1042
 
-b1.beers.create name:"Iso 3", style:"Lager"
-b1.beers.create name:"Karhu", style:"Lager"
-b1.beers.create name:"Tuplahumala", style:"Lager"
-b2.beers.create name:"Huvila Pale Ale", style:"Pale Ale"
-b2.beers.create name:"X Porter", style:"Porter"
-b3.beers.create name:"Hefeweizen", style:"Weizen"
-b3.beers.create name:"Helles", style:"Lager"
+beer1 = Beer.create name:"Iso 3", style:"Lager", brewery_id: brew1.id
+beer2 = Beer.create name:"Karhu", style:"Lager", brewery_id: brew1.id
+beer3 = Beer.create name:"Tuplahumala", style:"Lager", brewery_id: brew1.id
+beer4 = Beer.create name:"Huvila Pale Ale", style:"Pale Ale", brewery_id: brew2.id
+beer5 = Beer.create name:"X Porter", style:"Porter", brewery_id: brew2.id
+beer6 = Beer.create name:"Hefeweizen", style:"Weizen", brewery_id: brew3.id
+beer7 = Beer.create name:"Helles", style:"Lager", brewery_id: brew3.id
+
+Rating.create score:10, beer_id:beer1.id
+Rating.create score:9, beer_id:beer1.id
+Rating.create score:2, beer_id:beer2.id
+Rating.create score:3, beer_id:beer2.id
+Rating.create score:5, beer_id:beer3.id
+Rating.create score:8, beer_id:beer3.id
+Rating.create score:7, beer_id:beer4.id
+Rating.create score:7, beer_id:beer4.id
+Rating.create score:1, beer_id:beer5.id
+Rating.create score:1, beer_id:beer5.id
+Rating.create score:10, beer_id:beer6.id
+Rating.create score:10, beer_id:beer6.id
+Rating.create score:9, beer_id:beer7.id
+Rating.create score:9, beer_id:beer7.id
