681507237d4055f0f68cfecbf89d712a9ba84f15
Juha-Pekka Moilanen <juha-pekka.moilanen@helsinki.fi>
2017-01-28 15:05:12 +0200

    Add new rating feature

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,4 +2,14 @@ class RatingsController < ApplicationController
   def index
     @ratings = Rating.all
   end
+
+  def new
+    @rating = Rating.new
+  end
+
+  def create
+    #Rating.create beer_id: params[:rating][beer_id], score: params[:rating][:score]
+    Rating.create params.require(:rating).permit(:score, :beer_id)
+    redirect_to(ratings_path)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
 
   get 'ratings', to: 'ratings#index'
 
+  get 'ratings/new', to: 'ratings#new'
+
+  post 'ratings', to: 'ratings#create'
+
 end
