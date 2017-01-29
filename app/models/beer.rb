9217491cbb9da7b57754fc3f0ac5def960745832
Juha-Pekka Moilanen <juha-pekka.moilanen@helsinki.fi>
2017-01-29 23:06:04 +0200

    Add module for average_rating

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,23 +1,15 @@
 class Beer < ActiveRecord::Base
+  include RatingAverage
+
   belongs_to :brewery
   has_many :ratings
 
-  def average_rating
-    sum = 0
-    avg = 0
-    nOfRatings = self.ratings.count
-    if nOfRatings != 0
-      self.ratings.each {|ratingElement| sum += ratingElement.score}
-      avg = sum / (self.ratings.count.to_f)
-    end
-  end
-
-  # @return [Float]
-  def average_rating_with_map
-    self.ratings.map {|ratingElement|
-      ratingElement.score
-      }.sum.to_f / self.ratings.count
-  end
+  #{# @return [Float]
+  #def average_rating_with_map
+  #  self.ratings.map {|ratingElement|
+  #    ratingElement.score
+  #    }.sum.to_f / self.ratings.count
+  #end}
 
   def to_s
     self.name + " - " + self.brewery.name
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,6 +1,9 @@
 class Brewery < ActiveRecord::Base
+  include RatingAverage
+
   has_many :beers
   has_many :ratings, through: :beers
+
 
   def print_report
     puts self.name
