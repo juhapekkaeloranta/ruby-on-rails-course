eb23f51c2e85d6ad980ad15b739b71df9e9f0dd0
Juha-Pekka Moilanen <juha-pekka.moilanen@helsinki.fi>
2017-02-11 13:32:34 +0200

    Delete all

--- app/controllers/concerns/rating_average.rb
+++
@@ -1,9 +0,0 @@
-module RatingAverage
-  extend ActiveSupport::Concern
-
-  def average_rating
-    self.ratings.map {|ratingElement|
-      ratingElement.score
-    }.sum.to_f / self.ratings.count
-  end
-end
--- app/models/rating.rb
+++
@@ -1,7 +0,0 @@
-class Rating < ApplicationRecord
-  belongs_to :beer
-
-  def to_s
-    Beer.find(self.beer_id.to_s).name + " " + self.score.to_s
-  end
-end
